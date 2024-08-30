22e21233fe00975801f37fe91e2166b0a212154d
Nahuel Arrieta <nahuel.arrietamza@gmail.com>
2024-08-30 16:11:50 -0300

    minor fixes

--- cei-app/src/app/components/usersTable.tsx
+++ cei-app/src/app/components/usersTable.tsx
@@ -7,30 +7,28 @@ export default function UsersTable({users}: {users: any[]}) {
         padding: '10px 8px',
         borderRadius: '10px',
       }}>
-      <table style={{width: '100%'}}>
-      <thead className="font-bold">
-        <tr>
-          <td width={"20%"}>Nombre</td>
-          <td width={"20%"}>Apellido</td>
-          <td width={"40%"}>Email</td>
-          <td width={"20%"}>DNI</td>
-        </tr>
-      </thead>
-        <hr style={{ border: '1px solid black', width: '100%' }} />
-      <tbody>
-        {
-          users.map((user: any, key: any) => (
-            <tr key={key}>
-              <td>{user.name}</td>
-              <td>{user.lastname}</td>
-              <td>{user.email}</td>
-              <td>{user.idNumber}</td>
-            </tr>
-          ))
-        }
-      </tbody>
-    </table>
-    
+      <table className="w-full">
+        <thead className="w-full">
+          <tr className="font-bold  w-full border-b-1 py-1 border-gray-800">
+            <td className="w-1/5">Nombre</td>
+            <td className="w-1/5">Apellido</td>
+            <td className="w-2/5">Email</td>
+            <td className="w-1/5">DNI</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            users.map((user: any, key: any) => (
+              <tr key={key}>
+                <td>{user.name}</td>
+                <td>{user.lastname}</td>
+                <td>{user.email}</td>
+                <td>{user.idNumber}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
