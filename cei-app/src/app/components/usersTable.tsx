b08193ccbacd0294552ce1b3acdd3e745f98779a
Nahuel Arrieta <nahuel.arrietamza@gmail.com>
2024-09-04 19:59:49 -0300

    fix styles

--- cei-app/src/app/components/usersTable.tsx
+++ cei-app/src/app/components/usersTable.tsx
@@ -1,15 +1,9 @@
 export default function UsersTable({users}: {users: any[]}) {
   return (
-    <div 
-      style={{
-        width: '100%',
-        border: '1px solid #ccc',
-        padding: '10px 8px',
-        borderRadius: '10px',
-      }}>
+    <div className="w-full border-1 px-4 py-1 rounded-lg bg-white">
       <table className="w-full">
         <thead className="w-full">
-          <tr className="font-bold  w-full border-b-1 py-1 border-gray-800">
+          <tr className="font-bold w-full border-b-1 py-1 border-gray-600 h-10">
             <td className="w-1/5">Nombre</td>
             <td className="w-1/5">Apellido</td>
             <td className="w-2/5">Email</td>
@@ -19,7 +13,7 @@ export default function UsersTable({users}: {users: any[]}) {
         <tbody>
           {
             users.map((user: any, key: any) => (
-              <tr key={key}>
+              <tr key={key} className={`border-gray-300 h-10 ${key !== users.length - 1 ? 'border-b-1 ' : ''}`}>
                 <td>{user.name}</td>
                 <td>{user.lastname}</td>
                 <td>{user.email}</td>
--- cei-app/src/app/users/page.tsx
+++ cei-app/src/app/users/page.tsx
@@ -3,6 +3,18 @@ import UsersTable from "../components/usersTable";
 export default function UsersPage() {
   return (
     <main  style={{padding: "10px"}}>
+      <h1 className="text-2xl font-bold text-gray-900 m-2">
+        Usuarios
+      </h1>
+
+      {/* TODO: Fetch users from API */}
+      {/* TODO: Search bar */}
+      {/* TODO: Pagination */}
+      {/* TODO: Modal to create new user */}
+      {/* TODO: Modal to see user details */}
+      {/* TODO: Add filter of user type */}
+
+
       <UsersTable  
         users={[
           {
@@ -10,6 +22,24 @@ export default function UsersPage() {
             lastname: "Garcia",
             email: "gabriel.garcia@gmail.com",
             idNumber: "12345678"
+          },
+          {
+            name: "Pepe",
+            lastname: "Garcia",
+            email: "pepito.garcia@gmail.com",
+            idNumber: "1234568"
+          },
+          {
+            name: "Pepe",
+            lastname: "Garcia",
+            email: "pepito.garcia@gmail.com",
+            idNumber: "1234568"
+          },
+          {
+            name: "Pepe",
+            lastname: "Garcia",
+            email: "pepito.garcia@gmail.com",
+            idNumber: "1234568"
           },
           {
             name: "Pepe",
