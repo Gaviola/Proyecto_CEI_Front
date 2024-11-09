3a09dad717d27071c7edfad9f5d36c85ba53e396
Nahuel Arrieta <nahuel.arrietamza@gmail.com>
2024-11-09 18:42:29 -0300

    add session token

--- cei-app/src/services/loans.js
+++ cei-app/src/services/loans.js
@@ -1,61 +1,58 @@
 export const saveLoan = async (loanData) => {
-    const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/admin/loans`;
+  const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/admin/loans`;
 
-    console.log("Guardando préstamo:", loanData);
-    try {
-      const response = await fetch(apiUrl, {
-        method: "POST", // PUT para modificar, POST para nuevo
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
-        },
-        body: JSON.stringify(loanData),
-      });
+  console.log("Guardando préstamo:", loanData);
+  try {
+    const response = await fetch(apiUrl, {
+      method: "POST", // PUT para modificar, POST para nuevo
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("sessionToken")}`,
+      },
+      body: JSON.stringify(loanData),
+    });
 
-      if (!response.ok) {
-        const errorMessage = await response.text();
-        throw new Error(`Error al guardar el préstamo: ${errorMessage}`);
-      }
-
-      const savedLoanID = await response.json();
-      console.log("Préstamo guardado con éxito");
-      return savedLoanID;
-
-    } catch (error) {
-      console.error("Error en saveLoan:", error);
-      throw error; // Re-lanza el error para que handleSaveLoan lo maneje
+    if (!response.ok) {
+      const errorMessage = await response.text();
+      throw new Error(`Error al guardar el préstamo: ${errorMessage}`);
     }
-  };
+
+    const savedLoanID = await response.json();
+    console.log("Préstamo guardado con éxito");
+    return savedLoanID;
+  } catch (error) {
+    console.error("Error en saveLoan:", error);
+    throw error; // Re-lanza el error para que handleSaveLoan lo maneje
+  }
+};
 
 export const saveLoanItem = async (loanItemData) => {
-    const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/admin/loan-items`;
+  const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/admin/loan-items`;
 
-    console.log("Guardando ítem de préstamo:", loanItemData);
-    try {
-      const response = await fetch(apiUrl, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
-        },
-        body: JSON.stringify(loanItemData),
-      });
+  console.log("Guardando ítem de préstamo:", loanItemData);
+  try {
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("sessionToken")}`,
+      },
+      body: JSON.stringify(loanItemData),
+    });
 
-      if (!response.ok) {
-        const errorMessage = await response.text();
-        throw new Error(`Error al guardar el ítem del préstamo: ${errorMessage}`);
-      }
-
-      const savedLoanItemID = await response.json();
-      console.log("Ítem de préstamo guardado con éxito");
-      return savedLoanItemID;
-
-    } catch (error) {
-      console.error("Error en saveLoanItem:", error);
-      throw error;
+    if (!response.ok) {
+      const errorMessage = await response.text();
+      throw new Error(`Error al guardar el ítem del préstamo: ${errorMessage}`);
     }
-}
 
+    const savedLoanItemID = await response.json();
+    console.log("Ítem de préstamo guardado con éxito");
+    return savedLoanItemID;
+  } catch (error) {
+    console.error("Error en saveLoanItem:", error);
+    throw error;
+  }
+};
 
 export const fetchLoans = async () => {
   const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/admin/loans`;
@@ -64,19 +61,18 @@ export const fetchLoans = async () => {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
+        Authorization: `Bearer ${localStorage.getItem("sessionToken")}`,
       },
     });
     const data = await res.json();
 
-    console.log("loans:",data);
+    console.log("loans:", data);
 
     return data;
   } catch (error) {
     console.error("Error fetching loans:", error);
   }
 };
-
 
 export const updateLoan = async (loanData) => {
   const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/admin/loans/${loanData.id}`;
@@ -87,7 +83,7 @@ export const updateLoan = async (loanData) => {
       method: "PATCH",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
+        Authorization: `Bearer ${localStorage.getItem("sessionToken")}`,
       },
       body: JSON.stringify(loanData),
     });
@@ -99,9 +95,8 @@ export const updateLoan = async (loanData) => {
 
     console.log("Préstamo actualizado con éxito");
     //return response.json();
-
   } catch (error) {
     console.error("Error en updateLoan:", error);
     throw error;
   }
-}
+};
