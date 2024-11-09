a319fc89c5f7e4d90f63deec9319d63c04690aa0
Nahuel Arrieta <nahuel.arrietamza@gmail.com>
2024-11-09 16:37:01 -0300

    add sessionprovider wrapper

---
+++ cei-app/src/app/components/sessionProviderWrapper.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import { SessionProvider } from "next-auth/react";
+import React from "react";
+
+const SessionProviderWrapper = ({ children }: { children: React.ReactNode }) => {
+    return <SessionProvider>{children}</SessionProvider>;
+};
+
+export default SessionProviderWrapper;
